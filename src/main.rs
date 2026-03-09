c5ee9fce9b2ffba56f95bc471bce314396c71626
BerYoucef <berrife77@gmail.com>
2026-03-09 20:36:10 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable the rust level entry point
+
+use core::panic::PanicInfo;
+
+// this function called on panic
+#[panic_handler] // this is a lang item.
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
+
+// never change the _start name
+#[unsafe(no_mangle)]
+// entry point
+pub extern "C" fn _start() -> ! {
+    loop{}
+}
+
